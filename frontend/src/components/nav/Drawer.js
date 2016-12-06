a4b9e5bf0e0692b020e61aaacccd500c3fe58fac
Dale Race <dale.race@daugherty.com>
2016-12-06 15:02:43 -0600

    Add avatar to drawer

--- frontend/src/components/nav/Drawer.js
+++ frontend/src/components/nav/Drawer.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router'
 
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+import Avatar from 'material-ui/Avatar';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+
 
 export default class DrawerSimple extends React.Component {
 
@@ -18,8 +22,15 @@ export default class DrawerSimple extends React.Component {
           open={this.props.open}
           width={200}
           onRequestChange={this.props.handleClose} >
+          <List>
+            <ListItem
+              disabled={true}
+              leftAvatar={ <Avatar src="/images/default_avatar.jpg" /> } >
+              Default User
+            </ListItem>
           <Link to="/"><MenuItem onTouchTap={this.props.handleClose}>Home</MenuItem></Link>
           <Link to="/about"><MenuItem onTouchTap={this.props.handleClose}>About</MenuItem></Link>
+        </List>
         </Drawer>
       </div>
     );
