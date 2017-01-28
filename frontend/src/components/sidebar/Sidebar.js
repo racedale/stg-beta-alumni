51643a2ce37c32ab7c10b19343b7d9198e03ae82
Dale Race <race.dale@gmail.com>
2017-01-27 21:27:08 -0600

    Add newsletter links to the sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -27,7 +27,26 @@ class Sidebar extends React.Component {
   render() {
     return (
       <aside>
-        This is the sidebar content
+        <ul className="newsletters">
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Newsletter_Final.pdf">Fall 2016 Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Fall__15_Newsletter_1.pdf">Fall '15 Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Beta_NEWSLETTER_2014.pdf">Spring 2014 Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/AlumniNewsletterJan14.pdf">January 2014 Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Alumni_Winter_Newsletter_Dec2012.pdf">Winter 2012 Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/ALUMNI_NEWS_LETTER.pdf">Fall 2012 Newsletter</a>
+          </li>
+        </ul>
       </aside>
     )
   }
