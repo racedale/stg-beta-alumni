8e5ceb4ae4311c01a5860ab796921e5b8cdbfad4
Dale Race <race.dale@gmail.com>
2017-01-27 21:46:34 -0600

    Separate newsletters between active and alumni
    Might not even be how to sort them

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -27,6 +27,7 @@ class Sidebar extends React.Component {
   render() {
     return (
       <aside>
+        <h2>Chapter Newsletters</h2>
         <ul className="newsletters">
           <li>
             <a href="http://q.b5z.net/i/u/10103480/f/Newsletter_Final.pdf">Fall 2016 Newsletter</a>
@@ -45,6 +46,19 @@ class Sidebar extends React.Component {
           </li>
           <li>
             <a href="http://q.b5z.net/i/u/10103480/f/ALUMNI_NEWS_LETTER.pdf">Fall 2012 Newsletter</a>
+          </li>
+        </ul>
+
+      <h2>Alumni Newsletters</h2>
+        <ul className="newsletters">
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Beta_Nws_3-20111.doc">Beta Newsletter Summer 2011</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/Beta_Nws_2-2011.pdf">Spring 2011 Beta Alumni Newsletter</a>
+          </li>
+          <li>
+            <a href="http://q.b5z.net/i/u/10103480/f/TheBetaWinter2011newsletter.doc">Link to 2011 Winter Newsletter</a>
           </li>
         </ul>
       </aside>
