efccced3b2c24cc3edcc3c4275b1b9b7ad61f5a5
Race, Dale <Dale.Race@mastercard.com>
2017-04-22 11:37:54 -0500

    Add newsletters component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
@@ -7,15 +8,28 @@ import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewslettersComponent } from './newsletters/newsletters.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  {
+    path: 'newsletters',
+    component: NewslettersComponent,
+    data: { title: 'Heroes List' }
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    NewslettersComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule,
     MaterialModule
---
+++ frontend/src/app/newsletters/newsletters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stg-newsletters',
+  templateUrl: './newsletters.component.html',
+  styleUrls: ['./newsletters.component.scss']
+})
+export class NewslettersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
