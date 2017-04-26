ee08f6a006c4c0949e55595c915b087011099e0e
Race, Dale <Dale.Race@mastercard.com>
2017-04-26 17:01:32 -0500

    Set up newsletters page

--- frontend/src/app/newsletters/newsletters.component.ts
+++ frontend/src/app/newsletters/newsletters.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'stg-newsletters',
   templateUrl: './newsletters.component.html',
-  styleUrls: ['./newsletters.component.scss']
+  styleUrls: ['./newsletters.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NewslettersComponent implements OnInit {
 
