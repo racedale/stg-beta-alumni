8959e4171b7d8b6a0e10a927f1f7b338535e66d3
Dale Race <dale.race@daugherty.com>
2017-04-26 20:38:24 -0500

    Makes newsletters component use ngFor loop and disable ripple

--- frontend/src/app/newsletters/newsletters.component.ts
+++ frontend/src/app/newsletters/newsletters.component.ts
@@ -14,4 +14,16 @@ export class NewslettersComponent implements OnInit {
   ngOnInit() {
   }
 
+  newsletters = [
+    {"title": "Fall 2016 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Newsletter_Final.pdf"},
+    {"title": "Fall 2015 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Fall__15_Newsletter_1.pdf"},
+    {"title": "Fall 2014 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Fall_2014_-_Newsletter__Final_.pdf"},
+    {"title": "Spring 2014 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Beta_NEWSLETTER_2014.pdf"},
+    {"title": "January 2014 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/AlumniNewsletterJan14.pdf"},
+    {"title": "Winter 2012 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Alumni_Winter_Newsletter_Dec2012.pdf"},
+    {"title": "Fall 2012 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/ALUMNI_NEWS_LETTER.pdf"},
+    {"title": "Beta Newsletter Summer 2011", "link": "http://q.b5z.net/i/u/10103480/f/Beta_Nws_3-20111.doc"},
+    {"title": "Spring 2011 Beta Alumni Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/Beta_Nws_2-2011.pdf"},
+    {"title": "Winter 2011 Newsletter", "link": "http://q.b5z.net/i/u/10103480/f/TheBetaWinter2011newsletter.doc"}
+  ];
 }
