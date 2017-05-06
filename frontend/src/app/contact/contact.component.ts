401c9f879927d4781429c389fa98caed473a7f07
Dale Race <dale.race@daugherty.com>
2017-05-05 20:02:11 -0500

    Add house committee section

--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -41,6 +41,48 @@ export class ContactComponent implements OnInit {
       "profileImg": "src/assets/images/contact/profile-pic.svg"
     },
   ];
+  houseCommittee = [
+    {
+      "role": "Chair",
+      "firstName": "Richard",
+      "lastName": "Holmes",
+      "email": "rich1bigdog@aol.com",
+      "phone": "9703014154",
+      "profileImg": "src/assets/images/contact/profile-pic.svg"
+    },
+    {
+      "role": "Alumnus",
+      "firstName": "Rob",
+      "lastName": "Shults",
+      "email": "robshults@tx.rr.com",
+      "phone": "972404221",
+      "profileImg": "src/assets/images/contact/profile-pic.svg"
+    },
+    {
+      "role": "Alumnus",
+      "firstName": "Ken",
+      "lastName": "Slaughter",
+      "email": "ken.slaughter@mchsi.com",
+      "phone": "3193775920",
+      "profileImg": "src/assets/images/contact/profile-pic.svg"
+    },
+    {
+      "role": "Alumnus",
+      "firstName": "Carl",
+      "lastName": "Baldwin",
+      "email": "csbaldwin2513@sbcglobal.net",
+      "phone": "6603418174",
+      "profileImg": "src/assets/images/contact/profile-pic.svg"
+    },
+    {
+      "role": "Active",
+      "firstName": "Andrew",
+      "lastName": "Snider",
+      "email": "@truman.edu",
+      "phone": "3145419165",
+      "profileImg": "src/assets/images/contact/profile-pic.svg"
+    }
+  ];
 
   constructor() { }
 
