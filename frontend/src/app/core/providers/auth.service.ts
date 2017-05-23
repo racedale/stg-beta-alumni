af57ba6beba97dedb043067b1c6b1de78d545e6f
Race, Dale <Dale.Race@mastercard.com>
2017-05-23 15:17:40 -0500

    Capitalize O for rxjs/observable

--- frontend/src/app/core/providers/auth.service.ts
+++ frontend/src/app/core/providers/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
